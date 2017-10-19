fdd239f25d9673d9826ce1f2c8486b9ac8917143
Ke Wang <453587854@qq.com>
2017-10-19 09:51:27 +0800

    关键部位加上try catch

--- bot/src/methods/UserTask.js
+++ bot/src/methods/UserTask.js
@@ -13,7 +13,7 @@ const Op = Sequelize.Op;
 class UserTask {
   user_id;
   interval_id;
-  
+
   constructor(user_id) {
     this.user_id = user_id;
     this.run = this.run.bind(this);
@@ -38,45 +38,53 @@ class UserTask {
       let existingUrls = userConfig.rssFeedTorrents.map(_ => _.url);
       let allRssFeeds = (await Promise.all(userConfig.rssFeeds.map(FetchRssFeed)));
       for (let k = 0; k < allRssFeeds.length; k++) {
-        // 每一个rss源设置一个循环
-        let currentRssFeed = allRssFeeds[k];
-        let rssFeedTorrents = currentRssFeed.torrents.filter(_ => existingUrls.indexOf(_.url) < 0);
-        for (let i = 0; i < rssFeedTorrents.length; i++) {
-          let rssFeedTorrentItem = rssFeedTorrents[i];
-          let rssFeedTorrent = await RssFeedTorrent.create({
-            rss_feed_id: rssFeedTorrentItem.rss_feed_id,
-            status: RssFeedTorrentStatus.PENDING_DOWNLOAD,
-            url: rssFeedTorrentItem.url,
-            title: rssFeedTorrentItem.title,
-            pub_date: Date.parse(rssFeedTorrentItem.pubDate), //FIXME: parse失败？
-          });
-          let torrentData = (await DownloadAndParseTorrent(rssFeedTorrentItem.url));
+        try {
+          // 每一个rss源设置一个循环
+          let currentRssFeed = allRssFeeds[k];
+          let rssFeedTorrents = currentRssFeed.torrents.filter(_ => existingUrls.indexOf(_.url) < 0);
+          for (let i = 0; i < rssFeedTorrents.length; i++) {
+            try {
+              let rssFeedTorrentItem = rssFeedTorrents[i];
+              let rssFeedTorrent = await RssFeedTorrent.create({
+                rss_feed_id: rssFeedTorrentItem.rss_feed_id,
+                status: RssFeedTorrentStatus.PENDING_DOWNLOAD,
+                url: rssFeedTorrentItem.url,
+                title: rssFeedTorrentItem.title,
+                pub_date: Date.parse(rssFeedTorrentItem.pubDate), //FIXME: parse失败？
+              });
+              let torrentData = (await DownloadAndParseTorrent(rssFeedTorrentItem.url));
 
-          if (currentRssFeed.max_size_mb * 1024 * 1024 > torrentData.length) {
-            // 种子文件合适，正在添加
-            await rssFeedTorrent.update({
-              status: RssFeedTorrentStatus.PENDING_ADD,
-              file_size_kb: torrentData.length / 1024,
-            });
-            let addResult = await AddTorrentToQb(userConfig.boxConfig, rssFeedTorrentItem.url);
-            if (addResult) {
-              rssFeedTorrent.update({
-                status: RssFeedTorrentStatus.ADDED,
-                file_size_kb: torrentData.length / 1024,
-              });
-            } else {
-              rssFeedTorrent.update({
-                status: RssFeedTorrentStatus.ADD_FAILED,
-                file_size_kb: torrentData.length / 1024,
-              });
+              if (currentRssFeed.max_size_mb * 1024 * 1024 > torrentData.length) {
+                // 种子文件合适，正在添加
+                await rssFeedTorrent.update({
+                  status: RssFeedTorrentStatus.PENDING_ADD,
+                  file_size_kb: torrentData.length / 1024,
+                });
+                let addResult = await AddTorrentToQb(userConfig.boxConfig, rssFeedTorrentItem.url);
+                if (addResult) {
+                  rssFeedTorrent.update({
+                    status: RssFeedTorrentStatus.ADDED,
+                    file_size_kb: torrentData.length / 1024,
+                  });
+                } else {
+                  rssFeedTorrent.update({
+                    status: RssFeedTorrentStatus.ADD_FAILED,
+                    file_size_kb: torrentData.length / 1024,
+                  });
+                }
+              } else {
+                // 种子文件太大
+                await rssFeedTorrent.update({
+                  status: RssFeedTorrentStatus.FILTERED_OUT,
+                  file_size_kb: torrentData.length / 1024,
+                });
+              }
+            } catch (feedTorrentError) {
+              console.log("feedTorrentError", feedTorrentError);
             }
-          } else {
-            // 种子文件太大
-            await rssFeedTorrent.update({
-              status: RssFeedTorrentStatus.FILTERED_OUT,
-              file_size_kb: torrentData.length / 1024,
-            });
           }
+        } catch (rssFeedError) {
+          console.warn("rssFeedError", rssFeedError);
         }
       }
     } catch (exception) {
