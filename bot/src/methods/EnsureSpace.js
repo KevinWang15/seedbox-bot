7da5676de362209303225dbc111efa79db9983b3
Ke Wang <453587854@qq.com>
2017-10-20 09:35:12 +0800

    计算需要freeup的空间量

--- bot/src/methods/EnsureSpace.js
+++ bot/src/methods/EnsureSpace.js
@@ -1,11 +1,20 @@
 import { httpRequest } from "../components/Http";
+import { AutoDelConfig } from "../models/AutoDelConfig";
 import request from "request";
 import urlJoin from "url-join";
 
 const qbCookies = {};
 
 //TODO: 如果不可能释放足够的空间，则返回false
-async function EnsureSpace(boxConfig) {
+async function EnsureSpace(boxConfig, spaceToFreeUpGB) {
+  let maxAllowedUsage, totalFilesSize; // 单位均为GB
+
+  let autoDelConfig = await AutoDelConfig.find({ user_id: boxConfig.user_id });
+  if (!autoDelConfig)
+    return true;
+
+  maxAllowedUsage = autoDelConfig.max_disk_usage_size_gb;
+
   let result = await httpRequest({
     jar: qbCookies[boxConfig.url],
     url: urlJoin(boxConfig.url, '/query/torrents'),
@@ -15,13 +24,21 @@ async function EnsureSpace(boxConfig) {
   });
   if (!result.error) {
     let items = JSON.parse(result.body);
-    let totalTorrentSize = items.map(_ => _.size).reduce((a, b) => (a + b), 0) / 1024 / 1024 / 1024;
-    console.log("totalTorrentSize", totalTorrentSize);
+    totalFilesSize = items.map(_ => _.size).reduce((a, b) => (a + b), 0) / 1024 / 1024 / 1024;
+    if (totalFilesSize + spaceToFreeUpGB > maxAllowedUsage) {
+      let spaceToFreeUp = totalFilesSize + spaceToFreeUpGB - maxAllowedUsage;
+      return await freeUpSpace(boxConfig, items, spaceToFreeUp);
+    } else {
+      return true;
+    }
   } else {
     console.error("/query/torrents failed, box id " + boxConfig.id);
     return false;
   }
-  return true;
+}
+
+async function freeUpSpace(boxConfig, filesList, spaceToFreeUp) {
+
 }
 
 export { EnsureSpace };
