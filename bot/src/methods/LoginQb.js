3b6a2c574d29342b06d3a7ae5c59201976f3f8f9
Ke Wang <453587854@qq.com>
2017-10-21 09:33:41 +0800

    修复LoginQb

--- bot/src/methods/LoginQb.js
+++ bot/src/methods/LoginQb.js
@@ -1,18 +1,24 @@
 import { httpRequest } from "../components/Http";
 import { cookieJars } from "../mem/CookieJars";
 import request from "request";
+import urlJoin from "url-join";
 
+//TODO: 在调用处，LoginQb返回false时做相应处理
 async function LoginQb(boxConfig) {
   let cookieJar = request.jar();
-  await httpRequest({
+  let result = await httpRequest({
     jar: cookieJar,
-    url: boxConfig.url,
+    url: urlJoin(boxConfig.url, '/login'),
     form: { username: boxConfig.username, password: boxConfig.password },
     auth: { username: boxConfig.username, password: boxConfig.password },
     method: "POST",
   });
 
+  if (result.errors || result.response.statusCode !== 200) {
+    return false;
+  }
   cookieJars[boxConfig.url] = cookieJar;
+  return true;
 }
 
 export { LoginQb };
