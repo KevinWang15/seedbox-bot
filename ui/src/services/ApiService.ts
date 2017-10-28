ce625584d6cf06a1f9b2818656c76e189f9bf9df
Ke Wang <453587854@qq.com>
2017-10-28 22:48:55 +0800

    显示服务器传回的错误信息

--- ui/src/services/ApiService.ts
+++ ui/src/services/ApiService.ts
@@ -1,15 +1,19 @@
 import {server as serverConfig} from "../config.js";
 import urlJoin from "url-join";
 import axios from "axios";
+import swal from 'sweetalert2/dist/sweetalert2.all.min.js';
 
-function api(endpoint, params, {showError = false} = {}) {
+function api(endpoint, params, {showError = true} = {}) {
     console.log(showError);
     axios.post(urlJoin(serverConfig.url, endpoint), params)
         .then(function (response) {
             console.log(response);
         })
         .catch(function (error) {
-            console.log(error);
+            if (showError && error.response.data.errMsg) {
+                console.dir(error);
+                swal('出错了', error.response.data.errMsg, 'error');
+            }
         });
 }
 
