a8f2355634172b470486e9be3545f52dbff27ae9
Ke Wang <453587854@qq.com>
2017-10-29 08:10:27 +0800

    fix bug

--- bot/src/methods/FetchRssFeed.js
+++ bot/src/methods/FetchRssFeed.js
@@ -4,46 +4,51 @@ import FeedParser from 'feedparser';
 async function FetchRssFeed(rssFeed) {
   return new Promise(async (resolve, reject) => {
     let feedparser = new FeedParser({});
-    let rss = (await httpRequest({
-        url: rssFeed.url,
-        method: "GET",
-      })
-    ).body;
+    try {
+      let rss = (await httpRequest({
+          url: rssFeed.url,
+          method: "GET",
+        })
+      ).body;
 
-    feedparser.on('error', function (error) {
-      reject(error);
-    });
-
-    feedparser.on('finish', function () {
-      try {
-        let stream = this;
-        let rssItem;
-        let torrents = [];
-        while (rssItem = stream.read()) {
-          let torrentData = {};
-          if (rssItem.enclosures && rssItem.enclosures[0]) {
-            torrentData["url"] = rssItem.enclosures[0].url;
-          } else if (rssItem.link) {
-            torrentData["url"] = rssItem.link;
-          }
-          torrentData["title"] = rssItem.title;
-          torrentData["pubDate"] = rssItem.pubDate || ((new Date()).toISOString());
-          torrentData["rss_feed_id"] = rssFeed.id;
-          torrents.push(torrentData);
-        }
-
-        resolve({
-          ...rssFeed.dataValues,
-          torrents,
-        });
-      } catch (error) {
+      feedparser.on('error', function (error) {
         reject(error);
-      }
-    });
+      });
 
-    feedparser.write(rss, () => {
-      feedparser.end();
-    });
+      feedparser.on('finish', function () {
+        try {
+          let stream = this;
+          let rssItem;
+          let torrents = [];
+          while (rssItem = stream.read()) {
+            let torrentData = {};
+            if (rssItem.enclosures && rssItem.enclosures[0]) {
+              torrentData["url"] = rssItem.enclosures[0].url;
+            } else if (rssItem.link) {
+              torrentData["url"] = rssItem.link;
+            }
+            torrentData["title"] = rssItem.title;
+            torrentData["pubDate"] = rssItem.pubDate || ((new Date()).toISOString());
+            torrentData["rss_feed_id"] = rssFeed.id;
+            torrents.push(torrentData);
+          }
+
+          resolve({
+            ...rssFeed.dataValues,
+            torrents,
+          });
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      feedparser.write(rss, () => {
+        feedparser.end();
+      });
+
+    } catch (exception) {
+      reject(exception);
+    }
   });
 }
 
