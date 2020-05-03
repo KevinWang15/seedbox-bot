31454debd7f6fd0b69139fd1e52a4c8b7723a33c
Ke Wang <453587854@qq.com>
2020-05-03 19:06:46 +0800

    remove master - slave clustering since we are already using forever / pm2

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,60 +1,26 @@
-import cluster from "cluster";
 import "babel-polyfill";
-import { sequelize } from "./databaseConnection";
+import {sequelize} from "./databaseConnection";
 import "./models";
-import { ScanAndAddNewUsers } from "./methods/ScanAndAddNewUsers";
-import { Exception } from "./models/Exception";
-import { CheckIfHasSpace } from "./methods/FreeUpSpace";
-import { readCoreSettings } from "./utils/coreSettings";
-let coreSettings = readCoreSettings();
+import {ScanAndAddNewUsers} from "./methods/ScanAndAddNewUsers";
+import {Exception} from "./models/Exception";
+import {readCoreSettings} from "./utils/coreSettings";
 
+let coreSettings = readCoreSettings();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+sequelize.sync().then(() => {
+  setInterval(() => {
+    let coreSettings2 = readCoreSettings();
+    if (coreSettings2.$version !== coreSettings.$version) {
+      console.log("Core Settings changed, restarting..");
+      process.exit();
+    }
+  }, 10000);
 
-if (cluster.isMaster) {
-  cluster.fork();
-
-  cluster.on('exit', function(worker, code, signal) {
-    cluster.fork();
-  });
-}
-
-
-if (cluster.isWorker) {
-  sequelize.sync().then(() => {
-
-    setInterval(() => {
-      let coreSettings2 = readCoreSettings();
-      if (coreSettings2.$version !== coreSettings.$version) {
-        console.log("Core Settings changed, restarting..");
-        process.exit();
-      }
-    }, 10000);
-
-    // 每一分钟扫描一下users表，看是否有新的用户加入
-    ScanAndAddNewUsers();
-    setInterval(ScanAndAddNewUsers, (coreSettings.newUserScanInterval || 60) * 1000);
-  });
-}
-
-
-// async function test() {
-//   let box = await BoxConfig.find({
-//     where: {
-//       id: 1,
-//     },
-//   });
-//
-//   for (let i = 0; i < 5; i++) {
-//     console.log("CheckingIfHasSpace");
-//     let result = await CheckIfHasSpace(box, 300).then(_ => {
-//       console.log('hasSpace', _.hasSpace, 'spaceToFreeUp', _.spaceToFreeUp);
-//     });
-//
-//     console.log(result);
-//   }
-// }
-// test();
+  // 每一分钟扫描一下users表，看是否有新的用户加入
+  ScanAndAddNewUsers();
+  setInterval(ScanAndAddNewUsers, (coreSettings.newUserScanInterval || 60) * 1000);
+});
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
